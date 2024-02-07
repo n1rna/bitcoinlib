4916e82e921c87e01b3e310c602f8c5c63e724e8
nima <n7r.distributed.ai@gmail.com>
2024-02-07 19:12:16 +0100

    Fix repr function for wallet object

--- bitcoinlib/wallets.py
+++ bitcoinlib/wallets.py
@@ -1565,6 +1565,7 @@ class Wallet(object):
 
     def __repr__(self):
         db_uri = '' if not self.db_uri else self.db_uri.split('?')[0]
+
         if DEFAULT_DATABASE in db_uri:
             return "<Wallet(name=\"%s\")>" % self.name
         return "<Wallet(name=\"%s\", db_uri=\"%s\")>" % \
